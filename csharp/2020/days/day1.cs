c9ff5faa9d90e0a33b62438939a9a7bcd47dd12e
Craft <38158429+Craft485@users.noreply.github.com>
2021-03-07 20:56:20 -0500

    actually complete day 1

--- csharp/2020/days/day1.cs
+++ csharp/2020/days/day1.cs
@@ -4,14 +4,30 @@ using System.IO;
 namespace Advent_of_Code.Y2020 {
     class Day1 {
         static void Main(string[] args) {
-            // This is going up 2 dirs because the exe is going to be in ./bin
             string[] lines = File.ReadAllLines(@"../inputs/day1.txt");
+            Part1(lines);
+            Part2(lines);
+        }
+        static void Part1(string[] lines) {
             // Match every element to every other element
             for (int i = 0; i < lines.Length; i++) {
                 for (int j = 0; j < lines.Length; j++) {
                     if (Convert.ToInt32(lines[i]) + Convert.ToInt32(lines[j]) == 2020) {
-                        Console.WriteLine($"{lines[i]} + {lines[j]} = 2020\nIndices: {i}, {j}\nProduct: {i * j}");
-                        System.Environment.Exit(0);
+                        Console.WriteLine($"{lines[i]} + {lines[j]} = 2020\nIndices: {i}, {j}\nProduct: {Convert.ToInt32(lines[i]) * Convert.ToInt32(lines[j])}");
+                        return;
+                    }
+                }
+            }
+        }
+        static void Part2(string[] lines) {
+            // Match every element to every other element
+            for (int i = 0; i < lines.Length; i++) {
+                for (int j = 0; j < lines.Length; j++) {
+                    for (int k = 0; k < lines.Length; k++) {
+                        if (Convert.ToInt32(lines[i]) + Convert.ToInt32(lines[j]) + Convert.ToInt32(lines[k]) == 2020) {
+                            Console.WriteLine($"{lines[i]} + {lines[j]} + {lines[k]} = 2020\nIndices: {i}, {j}, {k}\nProduct: {Convert.ToInt32(lines[i]) * Convert.ToInt32(lines[j]) * Convert.ToInt32(lines[k])}");
+                            return;
+                        }
                     }
                 }
             }
