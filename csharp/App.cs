cf13ebeb2da7f879fc51ee23fdb5d5e22f0106f0
Craft <38158429+Craft485@users.noreply.github.com>
2021-03-07 20:06:36 -0500

    clean up App.cs

--- csharp/App.cs
+++ csharp/App.cs
@@ -5,13 +5,9 @@ using System.Diagnostics;
 namespace Advent_of_Code {
     class App {
         public static string[] dirs = Directory.GetDirectories(".");
-        //This eval function isn't valid
         static void Main(string[] args) {
-            // Advent_of_Code.Test.Main(args);
             string year = args[0];
             string day = args[1];
-            // Console.WriteLine("[{0}]", string.Join(", ", args));
-            // Console.WriteLine($"arg 0:{year}\narg 1:{day}");
             if (Array.Exists(dirs, e => e == $@".\{year}")) {
                 string[] files = Directory.GetFiles($@".\{year}\days");
                 // Console.WriteLine("[{0}]", string.Join(", ", files));
