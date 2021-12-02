3a24fd4844a80388a54a36ae369d1394dbb67460
Craft <38158429+Craft485@users.noreply.github.com>
2021-12-02 15:27:45 -0500

    Day 2 2021

---
+++ JS/2021/day2/challenge.1.js
@@ -0,0 +1,21 @@
+const { readFileSync:readFile } = require('fs')
+
+const input = readFile('./input.txt', { encoding: 'utf-8' }).toString('utf-8')
+
+const list = input.split('\n')
+let depth = 0
+let horizontalPos = 0
+for (let i = 0; i < list.length; i++) {
+    const dir = list[i].split(' ')[0]
+    const count = list[i].split(' ')[1]
+    if (dir.toLowerCase() === 'up') {
+        depth -= parseInt(count)
+    } else if (dir.toLowerCase() === 'down') {
+        depth += parseInt(count)
+    } else {
+        // Direction is forward
+        horizontalPos += parseInt(count)
+    }
+}
+
+console.log(`${depth} | ${horizontalPos} | ${depth * horizontalPos}`)
