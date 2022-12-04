aae2f367bbd57813b5341505157a1383ca5d5da9
Craft485 <colindavis5@gmail.com>
2022-12-03 19:22:27 -0500

    2022 Day 1 both parts

---
+++ JS/2022/Day01/Part1.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' })
+
+const elves = input.split(`\r\n\r\n`)
+const scores = []
+
+for (const elf of elves) {
+  const Calories = elf.split('\n')
+ 
+  const score = Calories.reduce( (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue.trim()) )
+ 
+  scores.push(parseInt(score))
+}
+
+scores.sort((a, b) => { return a - b })
+
+console.log(scores[scores.length - 1])
