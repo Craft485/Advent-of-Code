86a82e2cac79ec90c69377dcbdf1a9aab04e0431
Craft485 <colindavis5@gmail.com>
2022-12-04 12:06:28 -0500

    Day 4 both parts

---
+++ JS/2022/Day04/Part2.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' })
+
+const pairs = input.split(`${input.includes('\r') ? '\r' : '\n'}\n`)
+
+let finalCount = 0
+
+for (const pair of pairs) {
+    const ranges = pair.split(',')
+    const firstElfRanges = ranges[0].split('-')
+    const firstElfMin = parseInt(firstElfRanges[0])
+    const firstElfMax = parseInt(firstElfRanges[1])
+    const secondElfRanges = ranges[1].split('-')
+    const secondElfMin = parseInt(secondElfRanges[0])
+    const secondElfMax = parseInt(secondElfRanges[1])
+    finalCount += (
+        (firstElfMax >= secondElfMin && firstElfMin <= secondElfMin) ||
+        (firstElfMax >= secondElfMax && secondElfMax >= firstElfMin) ||
+        ((firstElfMin <= secondElfMin && firstElfMax >= secondElfMax) ||     
+         (secondElfMin <= firstElfMin && secondElfMax >= firstElfMax))
+    ) ? 1 : 0
+}
+
+console.info(finalCount)
