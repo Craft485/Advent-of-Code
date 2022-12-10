1bd7ebf57af469b5d83b305c0a4c89fdebcdcc0f
Craft485 <colindavis5@gmail.com>
2022-12-10 12:49:31 -0500

    Remove uneeded log in day 5 part 2

--- JS/2022/Day06/Part2.js
+++ JS/2022/Day06/Part2.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync('./input.txt', { encoding: 'utf-8' }).trim()
 
 for (let i = 0; i < input.length; i++) {
     const set = new Set(input.slice(i, i + 14))
-    console.log(set)
     if (set.size === 14) {
         console.info(i + 14)
         break
