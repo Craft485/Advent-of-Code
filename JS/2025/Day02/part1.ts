b73a2ea32359d3f5563508a221afeeeb8ead6434
Colin Davis <colindavis5@gmail.com>
2025-12-05 23:55:50 -0500

    Add 2025 day 2 part 1

---
+++ JS/2025/Day02/part1.ts
@@ -0,0 +1,25 @@
+import { readFile } from 'node:fs/promises'
+
+const testcase = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+
+async function main() {
+    const data = await readFile('./input.txt', { encoding: 'utf-8' })
+    const ranges: [lower: number, upper: number][] = data.trim().split(',').map(range => range.split('-').map(Number) as [number, number])
+    let result = 0
+    for (const [lower, upper] of ranges) {
+        let curr = lower
+        while (curr <= upper) {
+            const numDigits = `${curr}`.length
+            if (numDigits % 2 === 1) {
+                curr = 10 ** numDigits
+                continue
+            }
+            const str = `${curr}`
+            if (str.slice(0, Math.trunc(str.length / 2)) === str.slice(Math.trunc(str.length / 2))) result += curr
+            curr++
+        }
+    }
+    return result
+}
+
+console.log(await main())
